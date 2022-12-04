3067d0a91d7f4cb881eba87f46f78c3f4fdadbdd
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-12-04 12:31:59 +0100

    portfolio

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -13,6 +13,20 @@ def generic():
     return render_template('generic.html')
 
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        name = data['name']
+        message = data['message']
+        file = database.write(f'\n{email},{name},{message}')
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    return 'form submited elo'
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        print(data)
+        return redirect('/')
+    else:
+        return 'something went wrong'
