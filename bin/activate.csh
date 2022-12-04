8a8acd12c7ddfaab814d53e658ba730f5937a2d9
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-12-04 10:53:00 +0100

    portfolio project

--- bin/activate.csh
+++ bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/mikowhy/Documents/Developer/python/udemy/portfolio/venv"
+setenv VIRTUAL_ENV "/Users/mikowhy/Documents/Developer/python/udemy/projekt1/venv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
--- server.py
+++ server.py
@@ -7,17 +7,8 @@ app = Flask(__name__)
 def my_portfolio():
     return render_template('index.html')
 
-
-@app.route("/favicon.ico")
-def blog():
-    return "This are my test of this server"
-
-
-@app.route("/blog/2022/miso")
-def blog2():
-    return "This route is bout Miso"
+@app.route("/generic.html")
+def generic():
+    return render_template('generic.html')
 
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
