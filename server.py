341c2f3ce29f6c17787cf3990903d08f7ce9e1b7
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-12-05 16:40:26 +0100

    portfolio project

--- server.py
+++ server.py
@@ -14,14 +14,6 @@ def generic():
     return render_template('generic.html')
 
 
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        email = data['email']
-        name = data['name']
-        message = data['message']
-        file = database.write(f'\n{email},{name},{message}')
-
-
 def write_to_csv(data):
     with open('database.csv', newline='', mode='a') as database2:
         email = data['email']
@@ -43,4 +35,4 @@ def submit_form():
         except:
             return 'did not save to database'
     else:
-        return 'something went wrong'
+        return 'something went wrong!'
